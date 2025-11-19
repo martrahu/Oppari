b1cd370c45037ff938a7ddc4497d1ab384fc4deb
Märt R <martrahu@hotmail.com>
2025-11-19 16:38:46 +0200

    19.11.2025

--- 19.11.2025/README.md
+++ 19.11.2025/README.md
@@ -1,5 +1,14 @@
 # Oppari 19.11.2025
 
+dfAll.csv failissa on git:sta sennoprojekti/SENNOmeasurement/inputData human ja nothuman kansioissa olevat measurement csv failit kasattu yhteen failiin. Y sarakkeessä 1 indikoi human:ia ja 0 nothuman:ia. On poistettu sarakkeet joiden arvot ovat pysyvätsti 0 (esim 'S0-value1')
+
+dnn.py failissa on malli jossa em dataa käytetään kouluttamiseen. Mallin ylikouluttaminen ei onnistuu hyvin homogeenisen datan johdosta. Alla olevassa kuvassa näkyy  human (Y sarakkeessa 1) ja nothuman esimerkki sampleita. Huomataan että jotkut sarakkeet (esim S1-value2 ja S2-value2) indikoi aika hyvin onko kyseess
+ human vai not human. Siksi mallin ylikouluttaminen ei onnistuu. Mikäli testX täytetään satunnaisarvoilla onnistuu ylikouluttaminen odotetusti.
+
 
 <img src="alldata.png">
+
+Alla olevassa korrelaatiotaulukossakin näkyy että S1-value 2 ja S2-value2 korreloi kohtalaisen hyvin Y arvon kanssa. Mikäli korrelaatio on sen verran selvä/välitön ei vältämätä tarvitsee neuroverkkoa vaan saa käyttää myös sklearn classifiereita.
 <img src="corr.png">
+
+Data todennäköisesti tulee olemaan heterogeenisempi kun aletaan ottamaan mittauksia biologisesta kudoksesta.
---
+++ 19.11.2025/dnn.py
@@ -0,0 +1,60 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Input, Dense
+from keras.optimizers import  Adam
+from matplotlib import pyplot as plt
+
+## Ladataan tiedosto
+df=pd.read_csv('dfAll.csv')
+
+# Erotellaan X ja Y
+X = df.drop(['Y'], axis=1) 
+Y = df['Y']
+
+## Splitataan data
+trainX, testX, trainY, testY = train_test_split(X, Y, test_size=0.2, stratify=Y)
+
+## Skaalaus
+scaler = MinMaxScaler(feature_range=(-1, 1))
+trainX = scaler.fit_transform(trainX)
+testX=scaler.transform(testX)
+#testX=np.random.random(testX.shape) #randomilla ylioppiminen tapahtuu odotetusti ja accuracy odotetust 0,5 tasoa
+
+## Mallin rakennus
+model = Sequential([
+    Input(shape=(X.shape[1],)),
+    Dense(64, activation='relu'),
+    Dense(32, activation='relu'),
+    Dense(1, activation='sigmoid')
+])
+
+model.compile(
+    optimizer=Adam(learning_rate=0.001),
+    loss='binary_crossentropy',
+    metrics=['accuracy']
+)
+
+## Treenaaminen
+history=model.fit(
+    trainX, trainY,
+    #validation_data=(testX, testY),  #kokeilun vuoksi test laitettu validaatioksi
+    epochs=12,
+    batch_size=8,
+    verbose=1
+)
+
+
+"""plt.plot(history.history['loss'],'b',label='training')
+plt.plot(history.history['val_loss'],'r',label='val')
+plt.legend()
+plt.show()"""
+
+## Mallin evaluaatio
+loss, acc=model.evaluate(testX, testY)
+
+print(f"Loss: {loss}")
+print(f"Acc: {acc}")
